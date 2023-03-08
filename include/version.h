1c9939ae94a7ce891a0be4f51cafd546904005c6
JMGK (titan) <jmgk77@gmail.com>
2023-03-08 16:00:13 -0300

    version bump

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "7"
+  #define BUILD_NUMBER "13"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.9.7 - 2023-03-08 12:33:59.160077"
+  #define VERSION "v0.9.13 - 2023-03-08 15:57:46.036174"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v0.9.7"
+  #define VERSION_SHORT "v0.9.13"
 #endif
